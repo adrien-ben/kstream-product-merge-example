93390f16b3ccd760d1a4a930518b53ecd74d3ebd
Adrien Bennadji <adrien.bennadji@live.fr>
2020-09-08 07:09:43 +0200

    Use cascading joins to achieve the same goal

--- src/main/java/com/adrienben/demo/kstreamconnectionsaggregationexample/config/StreamConfig.java
+++ src/main/java/com/adrienben/demo/kstreamconnectionsaggregationexample/config/StreamConfig.java
@@ -12,11 +12,7 @@ import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.kstream.Consumed;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.Materialized;
-import org.apache.kafka.streams.kstream.Produced;
-import org.apache.kafka.streams.kstream.Repartitioned;
+import org.apache.kafka.streams.kstream.*;
 import org.apache.kafka.streams.state.KeyValueStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -30,102 +26,113 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 @RequiredArgsConstructor
 public class StreamConfig {
 
-	// Input topics
-	public static final String PRODUCT_DETAILS_TOPIC = "product_details";
-	public static final String PRICES_TOPIC = "prices";
-	public static final String SKU_DETAILS_TOPIC = "sku_details";
-	public static final String OFFER_DETAILS_TOPIC = "offer_details";
+    // Input topics
+    public static final String PRODUCT_DETAILS_TOPIC = "product_details";
+    public static final String PRICES_TOPIC = "prices";
+    public static final String SKU_DETAILS_TOPIC = "sku_details";
+    public static final String OFFER_DETAILS_TOPIC = "offer_details";
 
-	// Internals
-	private static final String PRICES_BY_PRODUCT_ID_REKEY_TOPIC = "prices_by_product_id_rekey";
-	private static final String SKU_DETAILS_BY_PRODUCT_ID_REKEY_TOPIC = "sku_details_by_product_id_rekey";
-	private static final String OFFER_DETAILS_BY_PRODUCT_ID_REKEY_TOPIC = "offer_details_by_product_id_rekey";
-	private static final String PRODUCT_STORE_NAME = "product_store";
+    // Internals
+    private static final String PRICES_BY_PRODUCT_ID_REKEY_TOPIC = "prices_by_product_id_rekey";
+    private static final String SKU_DETAILS_BY_PRODUCT_ID_REKEY_TOPIC = "sku_details_by_product_id_rekey";
+    private static final String OFFER_DETAILS_BY_PRODUCT_ID_REKEY_TOPIC = "offer_details_by_product_id_rekey";
+    private static final String PRODUCT_STORE_NAME = "product_store";
 
-	// Output topics
-	public static final String PRODUCTS_TOPIC = "products";
+    // Output topics
+    public static final String PRODUCTS_TOPIC = "products";
 
-	private final ObjectMapper mapper;
+    private final ObjectMapper mapper;
 
-	@Bean
-	public KStream<String, Product> kStream(StreamsBuilder streamBuilder) {
-		// First we define the various serialization/deserialization objects we will need
-		var stringSerde = Serdes.String();
-		var productDetailsSerde = jsonSerde(ProductDetails.class);
-		var productDetailsConsumed = Consumed.with(stringSerde, productDetailsSerde);
-		var priceSerde = jsonSerde(Price.class);
-		var priceConsumed = Consumed.with(stringSerde, priceSerde);
-		var skuDetailsSerde = jsonSerde(SkuDetails.class);
-		var skuDetailsConsumed = Consumed.with(stringSerde, skuDetailsSerde);
-		var offerDetailsSerde = jsonSerde(OfferDetails.class);
-		var offerDetailsConsumed = Consumed.with(stringSerde, offerDetailsSerde);
-		var productSerde = jsonSerde(Product.class);
-		var productProduced = Produced.with(stringSerde, productSerde);
+    @Bean
+    public KStream<String, Product> kStream(StreamsBuilder streamBuilder) {
+        // First we define the various serialization/deserialization objects we will need
+        var stringSerde = Serdes.String();
+        var productDetailsSerde = jsonSerde(ProductDetails.class);
+        var productDetailsConsumed = Consumed.with(stringSerde, productDetailsSerde);
+        var priceSerde = jsonSerde(Price.class);
+        var priceConsumed = Consumed.with(stringSerde, priceSerde);
+        var skuDetailsSerde = jsonSerde(SkuDetails.class);
+        var skuDetailsConsumed = Consumed.with(stringSerde, skuDetailsSerde);
+        var offerDetailsSerde = jsonSerde(OfferDetails.class);
+        var offerDetailsConsumed = Consumed.with(stringSerde, offerDetailsSerde);
+        var productSerde = jsonSerde(Product.class);
+        var productProduced = Produced.with(stringSerde, productSerde);
 
-		// Group prices
-		// - We first stream the prices
-		// - Then we change the key of the message using select key so we can 'join' prices with products
-		// - Before we can join though, we need to re-repartition the prices to make sure price and product
-		// with the same key end up in the same partitions.
-		// - We can then group the prices by key.
-		var groupedPrices = streamBuilder.stream(PRICES_TOPIC, priceConsumed)
-				.selectKey((key, price) -> price.getProductId())
-				.repartition(Repartitioned.<String, Price>as(PRICES_BY_PRODUCT_ID_REKEY_TOPIC)
-						.withKeySerde(stringSerde)
-						.withValueSerde(priceSerde))
-				.groupByKey();
+        // Group prices
+        // - We first stream the prices
+        // - Then we change the key of the message using select key so we can 'join' prices with products
+        // - Before we can join though, we need to re-repartition the prices to make sure price and product
+        // with the same key end up in the same partitions.
+        // - We can then group the prices by key.
+        var priceKTable = streamBuilder
+                .stream(PRICES_TOPIC, priceConsumed)
+                .selectKey((key, price) -> price.getProductId())
+                .repartition(Repartitioned.<String, Price>as(PRICES_BY_PRODUCT_ID_REKEY_TOPIC)
+                        .withKeySerde(stringSerde)
+                        .withValueSerde(priceSerde))
+                .toTable(materializedAs("price", priceSerde));
 
-		// Group offer details
-		// Same principe as the price grouping
-		var groupedOfferDetails = streamBuilder.stream(OFFER_DETAILS_TOPIC, offerDetailsConsumed)
-				.selectKey((key, offerDetails) -> offerDetails.getProductId())
-				.repartition(Repartitioned.<String, OfferDetails>as(OFFER_DETAILS_BY_PRODUCT_ID_REKEY_TOPIC)
-						.withKeySerde(stringSerde)
-						.withValueSerde(offerDetailsSerde))
-				.groupByKey();
+        // Group offer details
+        // Same principe as the price grouping
+        var offerDetailsKTable = streamBuilder
+                .stream(OFFER_DETAILS_TOPIC, offerDetailsConsumed)
+                .selectKey((key, offerDetails) -> offerDetails.getProductId())
+                .repartition(Repartitioned.<String, OfferDetails>as(OFFER_DETAILS_BY_PRODUCT_ID_REKEY_TOPIC)
+                        .withKeySerde(stringSerde)
+                        .withValueSerde(offerDetailsSerde))
+                .toTable(materializedAs("offerDetails", offerDetailsSerde));
 
-		// Group sku details
-		// Same principe as the price grouping
-		var groupedSkuDetails = streamBuilder.stream(SKU_DETAILS_TOPIC, skuDetailsConsumed)
-				.selectKey((key, skuDetails) -> skuDetails.getProductId())
-				.repartition(Repartitioned.<String, SkuDetails>as(SKU_DETAILS_BY_PRODUCT_ID_REKEY_TOPIC)
-						.withKeySerde(stringSerde)
-						.withValueSerde(skuDetailsSerde))
-				.groupByKey();
+        // Group sku details
+        // Same principe as the price grouping
+        var skuDetailsKTable = streamBuilder
+                .stream(SKU_DETAILS_TOPIC, skuDetailsConsumed)
+                .selectKey((key, skuDetails) -> skuDetails.getProductId())
+                .repartition(Repartitioned.<String, SkuDetails>as(SKU_DETAILS_BY_PRODUCT_ID_REKEY_TOPIC)
+                        .withKeySerde(stringSerde)
+                        .withValueSerde(skuDetailsSerde))
+                .toTable(materializedAs("skuDetails", skuDetailsSerde));
 
-		// Group sku details
-		// Same principe as the price grouping
-		var groupedProductDetails = streamBuilder.stream(PRODUCT_DETAILS_TOPIC, productDetailsConsumed)
-				.mapValues((id, productDetails) -> {
-					productDetails.setId(id);
-					return productDetails;
-				})
-				.groupByKey();
+        // Group sku details
+        // Same principe as the price grouping
+        var productDetailsKTable = streamBuilder
+                .stream(PRODUCT_DETAILS_TOPIC, productDetailsConsumed)
+                .mapValues((id, productDetails) -> {
+                    productDetails.setId(id);
+                    return productDetails;
+                })
+                .toTable(materializedAs("productDetails", productDetailsSerde));
 
-		// Now our parts are ready to be merged using the co-group operation.
-		// For each of our grouped stream we define how it's gonna be merged in the final Product.
-		// We also define how to create the initial Product.
-		// Finally we check that our product is complete before we send it to the output topic.
-		var products = groupedProductDetails
-				.<Product>cogroup((key, productDetails, product) -> product.mergeProductDetails(productDetails))
-				.cogroup(groupedSkuDetails, (key, skuDetails, product) -> product.mergeSkuDetails(skuDetails))
-				.cogroup(groupedOfferDetails, (key, offerDetails, product) -> product.mergeOfferDetails(offerDetails))
-				.cogroup(groupedPrices, (key, price, product) -> product.mergePrice(price))
-				.aggregate(Product::new,
-						Materialized.<String, Product, KeyValueStore<Bytes, byte[]>>as(PRODUCT_STORE_NAME)
-								.withKeySerde(stringSerde)
-								.withValueSerde(productSerde))
-				.toStream()
-				.filter((key, product) -> product.isComplete());
+        var productsWithSku = productDetailsKTable
+                .join(
+                        skuDetailsKTable,
+                        (productDetails, skuDetails) -> new Product()
+                                .mergeProductDetails(productDetails)
+                                .mergeSkuDetails(skuDetails),
+                        materializedAs("productWithSkus", productSerde));
 
-		products.to(PRODUCTS_TOPIC, productProduced);
+        var productWithSkuAndOffers = productsWithSku
+                .join(offerDetailsKTable, Product::mergeOfferDetails, materializedAs("productWithSkuAndOffers", productSerde));
 
-		return products;
-	}
+        var products = productWithSkuAndOffers
+                .join(priceKTable, Product::mergePrice, materializedAs(PRODUCT_STORE_NAME, productSerde))
+                .toStream()
+                .filter((key, product) -> product.isComplete());
 
-	private <T> Serde<T> jsonSerde(Class<T> targetClass) {
-		return Serdes.serdeFrom(
-				new JsonSerializer<>(mapper),
-				new JsonDeserializer<>(targetClass, mapper, false));
-	}
+        products.to(PRODUCTS_TOPIC, productProduced);
+
+        return products;
+    }
+
+
+    private static <T> Materialized<String, T, KeyValueStore<Bytes, byte[]>> materializedAs(String name, Serde<T> valueSerde) {
+        return Materialized.<String, T, KeyValueStore<Bytes, byte[]>>as(name)
+                .withKeySerde(Serdes.String())
+                .withValueSerde(valueSerde);
+    }
+
+    private <T> Serde<T> jsonSerde(Class<T> targetClass) {
+        return Serdes.serdeFrom(
+                new JsonSerializer<>(mapper),
+                new JsonDeserializer<>(targetClass, mapper, false));
+    }
 }
